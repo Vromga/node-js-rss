be1a5ffe8780a250781a14f64d50a598c258cdca
Mikita Maiseichyk <Doktornikita@hotmail.com>
2020-04-14 16:45:00 +0300

    feat: add connect to mongo in user module

--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -1,35 +1,20 @@
 const uuid = require('uuid');
 const mongoose = require('mongoose');
 
-// class User {
-//   constructor({
-//     id = uuid(),
-//     name = 'USER',
-//     login = 'user',
-//     password = 'P@55w0rd'
-//   } = {}) {
-//     this.id = id;
-//     this.name = name;
-//     this.login = login;
-//     this.password = password;
-//   }
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    login: String,
+    password: String,
+    _id: {
+      type: String,
+      default: uuid
+    }
+  },
+  { versionKey: false }
+);
 
-//   static toResponse(user) {
-//
-//   }
-// }
-
-const userSchema = new mongoose.Schema({
-  name: String,
-  login: String,
-  password: String,
-  _id: {
-    type: String,
-    default: uuid
-  }
-});
-
-userSchema.static.toResponse = user => {
+userSchema.statics.toResponse = user => {
   const { id, name, login } = user;
   return { id, name, login };
 };
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -3,7 +3,7 @@ const usersRepo = require('./user.memory.repository');
 const getAllUsers = () => usersRepo.getAllUsers();
 const createUser = user => usersRepo.createUser(user);
 const getUserById = id => usersRepo.getUserById(id);
-const updateUser = (id, body) => usersRepo.updateUserById(id, body);
+const updateUser = userToUpdate => usersRepo.updateUserById(userToUpdate);
 const deleteUserById = id => usersRepo.deleteUserById(id);
 
 module.exports = {
