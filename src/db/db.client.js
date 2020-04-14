2786209120dbe928b836041fcd27040fda08768a
Mikita Maiseichyk <Doktornikita@hotmail.com>
2020-04-14 12:57:40 +0300

    feat: add user model

--- src/db/db.client.js
+++ src/db/db.client.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { MONGO_CONNECTION_STRING } = require('../common/config');
+const User = require('../resources/users/user.model');
 
 const connectToDB = cb => {
   mongoose.connect(MONGO_CONNECTION_STRING, {
@@ -9,6 +10,11 @@ const connectToDB = cb => {
 
   const db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error: '));
+  db.dropDatabase();
+  User.insertMany([
+    { name: 'user', login: 'USER', password: 'PacmanColorHouseApple' },
+    { name: 'user2', login: 'USER2', password: 'PacmanColorHouseApple' }
+  ]);
   db.once('open', () => {
     console.log('we are connected!');
     cb();
